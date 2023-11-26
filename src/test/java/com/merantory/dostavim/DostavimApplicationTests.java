bbf0019dae77c53b6e5cacb28bca81f146f2e379
Stanislav Tyunin <tynin22@gmail.com>
2023-11-26 17:48:53 +0300

    Spring project init

---
+++ src/test/java/com/merantory/dostavim/DostavimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merantory.dostavim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DostavimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
