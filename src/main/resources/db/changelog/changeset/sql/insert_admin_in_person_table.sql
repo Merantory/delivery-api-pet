ba887044fc32a85b608b59a5b576bebf99721695
Stanislav Tyunin <tynin22@gmail.com>
2023-12-14 23:17:24 +0300

    Insert admin person into database migration

---
+++ src/main/resources/db/changelog/changeset/sql/insert_admin_in_person_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO person(email, password, name, phone_number, address, role)
+VALUES ('dostavimadmin@gmail.com', '$2a$10$igmBIpRAYh0qtEG0Gj4lg.0x1kOjdNa2r56qPAHZe4Su8g/EM7mH2', 'admin', '79095030515', 'admin address', 'ROLE_ADMIN');
