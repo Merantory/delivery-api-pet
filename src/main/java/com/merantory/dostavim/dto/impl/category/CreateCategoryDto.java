8a5a45f5cb4bd6e0fd25531763e249c8088a019d
Stanislav Tyunin <tynin22@gmail.com>
2023-12-16 20:38:30 +0300

    Update example name field example for CreateCategoryDto

--- src/main/java/com/merantory/dostavim/dto/impl/category/CreateCategoryDto.java
+++ src/main/java/com/merantory/dostavim/dto/impl/category/CreateCategoryDto.java
@@ -1,5 +1,6 @@
 package com.merantory.dostavim.dto.impl.category;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -12,5 +13,6 @@ import lombok.NoArgsConstructor;
 public class CreateCategoryDto {
     @NotEmpty(message = "Значение поля не должно быть пустым")
     @Pattern(regexp = "^[a-zA-Zа-яА-Я]+(?:\\s[a-zA-Zа-яА-Я]+)*$", message = "Поле содержит запрещенные символы")
+    @Schema(type = "string", example = "Бакалея")
     private String name;
 }
